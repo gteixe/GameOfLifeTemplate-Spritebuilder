926e2090baa255d8f14521f668364bfcb5268b54
gteixe <gteixe@mit.edu>
2015-01-04 23:35:56 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -0,0 +1,16 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Gabriel Teixeira on 1/4/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+@end
